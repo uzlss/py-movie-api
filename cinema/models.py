5ee9a16f057c13b3a1c07c99996ef70cad5cc032
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-02-02 16:13:42 +0200

    create api_views

--- cinema/models.py
+++ cinema/models.py
@@ -7,4 +7,4 @@ class Movie(models.Model):
     duration = models.IntegerField()
 
     def __str__(self):
-        return f"Movie: {self.title} (id={self.id})"
+        return f"Title: {self.title} (id={self.id})"
