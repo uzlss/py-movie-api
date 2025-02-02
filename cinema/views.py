ee0ab815c2ebc6406464a1eb0c201063fcf31121
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-02-02 16:22:12 +0200

    fix: handle bad requests in movie_list

--- cinema/views.py
+++ cinema/views.py
@@ -18,6 +18,7 @@ def movie_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
